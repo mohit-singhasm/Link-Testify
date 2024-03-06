08e5ddda8edacd75b87ece6568e5ad9291c76d6d
mohit-singhasm <mohit.singhasm@gmail.com>
2024-03-06 13:59:45 +0530

    Updated the method for writing json files

--- scraper/index.js
+++ scraper/index.js
@@ -1,6 +1,7 @@
 import puppeteer from "puppeteer";
 import { links } from "../database/database.js";
 import { startLoader } from "./loader.js";
+import * as fs from 'node:fs'
 
 // const link = 'https://www.canadacarcash.com/page-sitemap.xml';
 
@@ -8,6 +9,7 @@ import { startLoader } from "./loader.js";
 // const links = 'https://www.canadacarcash.com/car-title-loans-saskatchewan/'
 
 let result = [];
+let error12 = []
 
 let testScrape = async (url) => {
 
@@ -33,9 +35,13 @@ let testScrape = async (url) => {
       // Evaluate the page content in the browser context
       const para = await page.evaluate(() => {
         // Select all <ul> elements on the page
-        const ulElements = document.querySelectorAll('.citypgsect6  ul li');
+        const ulElements = document.querySelectorAll('.question-module ul li');
 
         // Convert NodeList to an array and return it
+        console.log(ulElements.forEach(ulElement => {
+          ulElement.style.border = "2px solid red";
+        }))
+
         return Array.from(ulElements).map(li => {
           const anchor = li.querySelector('a')
           const span = li.querySelector('span')
@@ -45,9 +51,14 @@ let testScrape = async (url) => {
             return true;
           }
         });
+
       })
 
+      // console.log(para, "paraResult")
+
       let para1 = para.reduce((acc, currentValue) => Boolean(acc + currentValue), false);
+      // console.log(para1, 'para1')
+      // console.log(para)
 
       // console.log(para1)
       if (para1) {
@@ -55,18 +66,25 @@ let testScrape = async (url) => {
       }
 
     } catch (error) {
-      console.error('Not Working')
+      error12.push(link)
+      console.error('Not Working', error12)
     } finally {
       await page.close();
-      // console.log(result, )
     }
   }
   await browser.close()
 }
 
 
+async function writingResult (res) {
+  const jsonData = await JSON.stringify(result);
+  fs.writeFileSync('Results/Result.json', jsonData)
+}
+
+
 async function tester() {
   await testScrape(links)
+  await writingResult(result)
   startLoader(result)
 }
 
